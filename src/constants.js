556d4a463a43945b6fc0980dca56f8e88e70b3c9
SinthujanSinthu <sinthujan@ceylonsolutions.com>
2021-10-03 22:48:07 +0530

    Add common token for read public repo

--- src/constants.js
+++ src/constants.js
@@ -27,7 +27,7 @@ import { css } from "@emotion/react";
  `;
 
 export const GITHUB_GRAPH_QL_DATA={
-TOKEN:'Bearer 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', //The token can be only used for Reading public data in Github.
+TOKEN:'Bearer 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', //The token can be only used for Reading public data in Github.
 GITHUB_GRAPH_QL_API:'https://api.github.com/graphql', // The Github graph ql api
 };
 
